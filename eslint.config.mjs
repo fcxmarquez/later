665439edaf6555789f711fe90785c9745ae117aa
fcxmarquez <francisco.marquez.solt@gmail.com>
2026-07-26 17:07:38 -0600

    chore(tooling): align Next.js project configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,14 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const compat = new FlatCompat({ baseDirectory: __dirname });
-export default [...compat.extends("next/core-web-vitals", "next/typescript")];
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+
+export default defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+]);
